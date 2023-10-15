2bfa285150a5d4c787f6baf5272fc00d7981f901
iksin1706 <49130419+iksin1706@users.noreply.github.com>
2023-10-15 19:46:29 +0200

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <style>
+        </style>
+
+        @vite(['resources/scss/app.scss','resources/css/app.css','resources/js/app.js'])
+
+    </head>
+    <body>
+           
+        <div class="container">
+            <h1>Hello Sass</h1>
+        </div>
+
+    </body>
+</html>
