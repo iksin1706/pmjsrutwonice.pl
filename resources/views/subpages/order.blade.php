f4badb0a44ddfbceb266fe0b79b907dfac6d0001
iksin1706 <49130419+iksin1706@users.noreply.github.com>
2023-12-11 16:37:03 +0100

    feature: how to order

--- resources/views/subpages/order.blade.php
+++ resources/views/subpages/order.blade.php
@@ -11,7 +11,7 @@
     <meta name="color-scheme" content="dark">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    @vite(['resources/scss/app.scss','resources/js/preloader.js', 'resources/js/nav.js'])
+    @vite(['resources/scss/app.scss', 'resources/js/preloader.js', 'resources/js/nav.js'])
     <style>
         body {
             background: radial-gradient(circle at 0 100%, #20201e, #000) !important;
@@ -22,8 +22,95 @@
 <body>
     <x-navigation fontClass="color--white"></x-navigation>
     <x-preloader></x-preloader>
-    <section class="section" style="min-height: 100vh">
+    <section class="section pt-4 pb-3 d-flex flex-center" style="min-height: 100vh; max-width: 1600px w-100">
+        <h1 class="heading-2 pb-3">Jak zamówić?</h1>
+        <div class="d-flex w-100 flex-center">
+            <ol class="step-list">
+                <li class="step-list__item">
+                    <div class="step-list__item__inner">
+                        <div class="content">
+                            <div class="body">
+                                <h2>Napisz lub zadzwoń</h2>
+                                <p>Skontaktuj się z nami przedstawiając jakiego urządzenia potrzebujesz oraz czym sie
+                                    zajmujesz tak abyśmy mogli przedstawić ci oferte dostosowaną do twoich potrzeb. <br>
+                                    Aby to zrobić <a class="link" href="">Kliknij tutaj</a></p>
+                            </div>
+                            <div class="icon">
+                                <img src="{{ asset('img/icons/phone-icon.svg') }}" alt="Check" />
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="step-list__item">
+                    <div class="step-list__item__inner">
+                        <div class="content">
+                            <div class="body">
+                                <h2>Zapoznaj się z oferta</h2>
+                                <p>Na maila otrzymasz oferte z aktualnymi cenami urządzenia którymi jesteś
+                                    zainteresowany.
+                                </p>
+                            </div>
 
+                            <div class="icon">
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/13060/mail_outline_copy.svg"
+                                    alt="Check" />
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="step-list__item">
+                    <div class="step-list__item__inner">
+                        <div class="content">
+                            <div class="body">
+                                <h2>Umów się na wizyte</h2>
+                                <p> Jeżeli chcesz możesz się umówić na wizyte i zobaczyć oraz przetestować nasze
+                                    urządzenia na żywo. Oczywiście o ile posiadamy taką na stanie:)</p>
+                            </div>
+
+                            <div class="icon">
+                                <img src="{{ asset('img/icons/person-icon.svg') }}"
+                                    alt="Check" />
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="step-list__item">
+                    <div class="step-list__item__inner">
+                        <div class="content">
+                            <div class="body">
+                                <h2>Zamów urządzenie</h2>
+                                <p> A my odrazu po złożeniu zamówienia przystąpimy do jego produkcji. Czas oczekiwania
+                                    jest
+                                    zależny od urządzenia i ilości zamówień jednak jest o zwykle około miesiąca</p>
+                            </div>
+
+                            <div class="icon">
+                                <img src="{{ asset('img/icons/order-icon.svg') }}"
+                                    alt="Check" />
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="step-list__item">
+                    <div class="step-list__item__inner">
+                        <div class="content">
+                            <div class="body">
+                                <h2>Dostawa</h2>
+                                <p>
+                                    Gdy urządzenie bedzie gotowe oraz wszelkie formalności dopełnione zapakujemy je i
+                                    wyślemy do ciebie.
+                                </p>
+                            </div>
+
+                            <div class="icon">
+                                <img src="{{ asset('img/icons/delivery-icon.svg') }}"
+                                    alt="Check" />
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ol>
+        </div>
     </section>
     @include('layout/footer')
 </body>
